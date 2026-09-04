2a020806404a008fd70aeced7b9708887380d7a4
Felipe Francisco Burzlaff <20251CTI-NH0033@nh.ifsul.edu.br>
2026-09-04 10:27:05 -0300

    Link para MVP.css e criação de connect.php em src/

--- Public/index.php
+++ Public/index.php
@@ -11,6 +11,7 @@ echo <<<HTML
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>loxja</title>
+        <link href="MVP.css" rel="stylesheet">
         <link href="style_index.css" rel="stylesheet">
     </head>
     <body>
