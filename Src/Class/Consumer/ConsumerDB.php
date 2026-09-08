e1ad8a26474cbaf6cd5c2947545b9c63079e0449
Arthur de Oliveira Rabello <20251CTI-NH0060@nh.ifsul.edu.br>
2026-09-08 11:17:19 -0300

    costumer class finished

---
+++ Src/Class/Consumer/ConsumerDB.php
@@ -0,0 +1,36 @@
+<?php
+
+class ConsumerDB
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getConsumerByID(int $id): ?Consumer
+    {
+        $stmt = $this->pdo->prepare("SELECT clt_email,clt_name,clt_id FROM consumers WHERE id = ?");
+        $stmt->execute([$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            return new Consumer($row['clt_id'], $row['clt_name'], $row['clt_email']);
+        }
+
+        return null;
+    }
+    public function getConsumerByEmail(string $email): ?Consumer
+    {
+        $stmt = $this->pdo->prepare("SELECT clt_email,clt_name,clt_id FROM consumers WHERE clt_email = ?");
+        $stmt->execute([$email]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            return new Consumer($row['clt_id'], $row['clt_name'], $row['clt_email']);
+        }
+
+        return null;
+    }
+}
