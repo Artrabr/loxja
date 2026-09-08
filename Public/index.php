ed003fbca2dee1774698e38e2aa8b6410449c7ad
Felipe Francisco Burzlaff <20251CTI-NH0033@nh.ifsul.edu.br>
2026-09-08 09:40:17 -0300

    update no index.php

--- Public/index.php
+++ Public/index.php
@@ -8,10 +8,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>loxja</title>
-        <link href="MVP.css" rel="stylesheet">\
+        <link href="MVP.css" rel="stylesheet">
         <link href="style_index.css" rel="stylesheet">
     </head>
     <body>
-        <h1>rabello gay</h1>
+        <header>
+            <p>cafe</p> <!--- logo + nome da loja --->
+            <p>produtos</p> <!--- leva pra outra página --->
+            <p>sobre nós</p> <!--- leva pra outra página --->
+            <p><!-- verificar se usuário já está logado para
+                definir entre registrar/logar ou deslogar --></p>
+        </header>
     </body>
 </html>
