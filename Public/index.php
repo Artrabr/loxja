8e4c8dcdc74d4af5ac306c9dc798d7edc80e39d6
Kléberson Rafael Vargas dos Santos <20251CTI-NH0029@nh.ifsul.edu.br>
2026-09-24 09:36:18 -0300

    foto sem perfil feita tropa

--- Public/index.php
+++ Public/index.php
@@ -401,9 +401,8 @@ function formatarPreco(float $preco): string
                     <li><a href="#contato">Contato</a></li>
                 </ul>
                 <?php
-                    $logado = false;
-                    if($logado){
-                        echo "<a class='login-button' href='Client/index.php'>Area do Cliente</a>";
+                    if ($success = "login_success"){
+                        echo "<img src='perfilsemfoto.png' alt='User' class='user-icon'>";
                     } else {
                         echo "<a class='login-button' href='Login/index.php'>Login</a>";
                     }
